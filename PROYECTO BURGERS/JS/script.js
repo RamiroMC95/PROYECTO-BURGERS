2c2658a6b7572c208da7642a07b44ce9cdcfd572
RamiroMC95 <128424643+RamiroMC95@users.noreply.github.com>
2023-05-01 20:56:47 -0300

    Add files via upload

--- PROYECTO BURGERS/JS/script.js
+++ PROYECTO BURGERS/JS/script.js
@@ -1,18 +1,91 @@
-alert ("Bienvenido a Hamburguesas Los Pochos, estamos listos para tomar tu pedido!")
+// alert ("Bienvenido a Hamburguesas Los Pochos, estamos listos para tomar su pedido")
 
-let Nombre = prompt ("Como te llamas?")
+// let nombre = prompt ("Como te llamas?")
 
-let Direccion = prompt ("Donde enviamos tu pedido?")
+// let direccion = prompt ("Donde enviamos tu pedido?")
 
-let PrecioLenteja = 200
-let PrecioEspinaca=250
+// let precioLenteja = 200
+// let precioEspinaca=250
 
-let CantidadLenteja = prompt ("Cuantas hambuguesas de lentejas queres?")
-let CantidadEspinaca = prompt ("Cuantas hambuguesas de espinacas queres?")
+// let cantidadLenteja = prompt ("Cuantas hambuguesas de lentejas queres?")
+// let cantidadEspinaca = prompt ("Cuantas hambuguesas de espinacas queres?")
 
-let TotalLenteja = CantidadLenteja * PrecioLenteja
-let TotalEspinaca = CantidadEspinaca * PrecioEspinaca
+// let totalLenteja = cantidadLenteja * precioLenteja
+// let totalEspinaca = cantidadEspinaca * precioEspinaca
 
-let Total= TotalLenteja+TotalEspinaca
+// let total= totalLenteja + totalEspinaca
 
-alert ("El costo total es de " +Total)
+// alert ("El costo total es de " +total)
+
+
+
+
+
+// Definimos los productos disponibles
+debugger
+const productos = [
+    {
+      nombre: "Lentejas",
+      precio: 100,
+    },
+    {
+      nombre: "Espinaca",
+      precio: 150,
+    },
+    {
+      nombre: "Remolacha",
+      precio: 125,
+    },
+  ];
+  
+  // Definimos la función para agregar productos al carrito
+  function agregarAlCarrito() {
+    // Creamos el carrito vacío
+    const carrito = [];
+  
+    // Preguntamos al usuario qué productos desea agregar al carrito y cuántas unidades de cada uno
+    let seguirAgregando = true;
+  
+    while (seguirAgregando) {
+      // Mostramos los productos disponibles para que el usuario elija
+      console.log("Productos disponibles:");
+      for (let i = 0; i < productos.length; i++) {
+        console.log(`${i + 1}. ${productos[i].nombre} - $${productos[i].precio}`);
+      }
+  
+      // Pedimos al usuario que seleccione un producto
+      const seleccion = prompt("Seleccione un producto (1-3)");
+  
+      // Verificamos si la selección es válida
+      if (seleccion >= 1 && seleccion <= 3) {
+        // Pedimos al usuario que ingrese la cantidad de unidades que desea del producto seleccionado
+        const cantidad = parseInt(prompt(`Ingrese la cantidad de ${productos[seleccion - 1].nombre} que desea agregar`));
+  
+        // Verificamos si la cantidad ingresada es válida
+        if (cantidad > 0) {
+          // Agregamos el producto al carrito con la cantidad seleccionada
+          const producto = productos[seleccion - 1];
+          carrito.push({ ...producto, cantidad: cantidad });
+          console.log(`Se agregaron ${cantidad} ${producto.nombre}(s) al carrito`);
+        } else {
+          console.log("Cantidad no válida");
+        }
+      } else {
+        console.log("Selección no válida");
+      }
+  
+      // Preguntamos al usuario si desea agregar otro producto
+      const respuesta = prompt("¿Desea agregar otro producto? (s/n)").toLowerCase();
+      seguirAgregando = respuesta === "s";
+    }
+  
+    // Mostramos el contenido del carrito
+    console.log("Contenido del carrito:");
+    for (let i = 0; i < carrito.length; i++) {
+      console.log(`${carrito[i].nombre} x ${carrito[i].cantidad} = $${carrito[i].precio * carrito[i].cantidad}`);
+    }
+  }
+  
+  // Llamamos a la función para agregar productos al carrito
+  agregarAlCarrito();
+  
