186df5027ea285424bb5581f56ba3a62a23c6ed4
RamiroMC95 <128424643+RamiroMC95@users.noreply.github.com>
2023-05-07 20:27:33 -0300

    Add files via upload

--- PROYECTO BURGERS/JS/script.js
+++ PROYECTO BURGERS/JS/script.js
@@ -38,30 +38,30 @@ const productos = [
     },
   ];
   
-  // Definimos la función para agregar productos al carrito
+  // definimos la función para agregar productos al carrito
   function agregarAlCarrito() {
     // Creamos el carrito vacío
     const carrito = [];
   
-    // Preguntamos al usuario qué productos desea agregar al carrito y cuántas unidades de cada uno
+    // preguntamos al usuario qué productos desea agregar al carrito y cuántas unidades de cada uno
     let seguirAgregando = true;
   
     while (seguirAgregando) {
-      // Mostramos los productos disponibles para que el usuario elija
+      // mostramos los productos disponibles para que el usuario elija
       console.log("Productos disponibles:");
       for (let i = 0; i < productos.length; i++) {
         console.log(`${i + 1}. ${productos[i].nombre} - $${productos[i].precio}`);
       }
   
-      // Pedimos al usuario que seleccione un producto
+      // ppedimos al usuario que seleccione un producto
       const seleccion = prompt("Seleccione un producto (1-3)");
   
-      // Verificamos si la selección es válida
+      // verificamos si la selección es válida
       if (seleccion >= 1 && seleccion <= 3) {
-        // Pedimos al usuario que ingrese la cantidad de unidades que desea del producto seleccionado
+        // se pide al usuario que ingrese la cantidad de unidades que desea del producto seleccionado
         const cantidad = parseInt(prompt(`Ingrese la cantidad de ${productos[seleccion - 1].nombre} que desea agregar`));
   
-        // Verificamos si la cantidad ingresada es válida
+    
         if (cantidad > 0) {
           // Agregamos el producto al carrito con la cantidad seleccionada
           const producto = productos[seleccion - 1];
@@ -74,7 +74,7 @@ const productos = [
         console.log("Selección no válida");
       }
   
-      // Preguntamos al usuario si desea agregar otro producto
+      // se le pregunta al usuario siquiere agregar otro producto
       const respuesta = prompt("¿Desea agregar otro producto? (s/n)").toLowerCase();
       seguirAgregando = respuesta === "s";
     }
